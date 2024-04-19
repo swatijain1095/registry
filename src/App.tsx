c328bacd7f1a310ae5fcbc0864f7998e5a04c46d
Swati Jain <jain.swati71@gmail.com>
2024-04-19 11:20:40 +0200

    React Router setup and navigation with a Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailsPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>hi there</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
